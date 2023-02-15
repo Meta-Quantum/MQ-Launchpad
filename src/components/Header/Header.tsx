b4f3d62b4d08db52988e422d62002f2b3e03d247
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-02-15 12:57:14 +0200

    Update: Finished the navbar section of the header. Only design wise.
    Functionality tbd.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import './Header.scss'
+
+function Header() {
+  return (
+    <div className='header__container'>
+     <Navbar />
+
+    </div>
+  )
+}
+
+export default Header
