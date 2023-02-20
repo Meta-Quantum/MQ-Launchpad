9f623e97c140c8dbed069ec21202d46e789be2e6
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-02-20 14:21:07 +0200

    Update: finished the mock-up component for the crypto carousel.
    At the moment I only used some mock-up data stored locally but we will use the CoinMarketplace API for the final implementation.

--- initialBoilerPlate/src/app.tsx
+++ initialBoilerPlate/src/app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.scss'
+import CryptoCarousel from "./components/CryptoCarousel/CryptoCarousel";
 import Header from "./components/Header/Header";
 import Video from "./components/Video/Video";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <Header/>
      <Video />
+     <CryptoCarousel />
      App
     </div>
   );
