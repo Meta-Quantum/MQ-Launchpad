b011f8d989c52100f8fb798a9a19773f073d1321
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-02-24 04:30:14 +0200

    Another small fix regarding responsiveness.

--- initialBoilerPlate/src/components/ApplyLaunchpad/ApplyLaunchpad.tsx
+++ initialBoilerPlate/src/components/ApplyLaunchpad/ApplyLaunchpad.tsx
@@ -10,10 +10,11 @@ function ApplyLaunchpad() {
     <p className='apply__container__wrap__element__text__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna 
         aliqua. Est lorem ipsum dolor sit amet. Sed adipiscing 
-        diam donec adipiscing tristique. Dolor purus non enim praesent 
+        {/* diam donec adipiscing tristique. Dolor purus non enim praesent 
         elementum facilisis leo vel fringilla. Mi sit amet mauris commodo 
         quis imperdiet massa tincidunt nunc. Et egestas quis ipsum suspendisse 
-        ultrices.</p>
+        ultrices.</p> */}
+        </p>
 </div>
 
 
