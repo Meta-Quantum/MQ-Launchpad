52c016a39a75176f86afc10e9d19d3072e78b069
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-03-16 13:41:39 +0200

    Cleaned the old docs out of the project

--- src/components/Navbar/Buttons/Buttons.tsx
+++ src/components/Navbar/Buttons/Buttons.tsx
@@ -1,21 +1,69 @@
-import React from 'react'
+import React from 'react';
 import loginIcon from '../../../assets/login-icon.png'
 import walletIcon from '../../../assets/wallet-icon.png'
 import './Buttons.scss'
+import { useNavigate } from 'react-router'
+import {useState, useEffect} from 'react'
+import WalletConnect from '../../WalletConnect/WalletConnect';
+import Web3 from 'web3'
+import { Web3ReactProvider } from '@web3-react/core'
+import { useWeb3React } from "@web3-react/core"
+
 
 function Buttons() {
+  const {deactivate} = useWeb3React()
+  function getLibrary(provider:any) {
+    return new Web3(provider)
+  }
+
+ const navigate = useNavigate()
+const redirectDashboard= () =>  navigate('/dash')
+const [showModal, setShowModal] = useState(false)
+const openModal = ()  => setShowModal(true)
+const loggedAccount = localStorage.getItem('account');
+
+useEffect(() => {
+  if (showModal === true) {
+    document.body.style.overflow = 'hidden'
+  } else {
+    document.body.style.overflow = 'unset'
+  }
+}, [showModal])
+
+async function disconnect() {
+  try{
+  await deactivate()
+   console.log('Disconnected')
+   localStorage.clear();
+   navigate('/')
+   }
+  catch(e) {
+   console.log(e)
+   }
+   }
+
+
   return (
+    
     <div className='buttons__container'>
         <div className='buttons__container__element'>
-            <img className='buttons__container__element__icon' src={loginIcon}  alt='login-icon'/>
-            <input className='buttons__container__element__login' type='button' value='login'/>
+        { (loggedAccount != null) ?  <input className='buttons__container__element__dashboard' type='button' value='dashboard' onClick={redirectDashboard}/> :
+           <input className='buttons__container__element__login' type='button' value='login' />
+          }
+            
         </div>
         <div className='buttons__container__element'>
-            <img className='buttons__container__element__icon' src={walletIcon} alt='wallet-icon'/>
-            <input className='buttons__container__element__wallet' type='button' value='wallet connect' />
+          { (loggedAccount != null) ?  <div className='buttons__container__element__connected'>
+            <span className='buttons__container__element__connected__address'>{loggedAccount}</span>
+           
+            <input className='buttons__container__element__wallet' type='button' value='disconnect' onClick={disconnect} ></input>
+            </div> : <input className='buttons__container__element__wallet' type='button' value='wallet connect' onClick={openModal} />
+          }
+           
         </div>
-
-
+        <Web3ReactProvider getLibrary={getLibrary}>
+<WalletConnect showModal={showModal}  setShowModal={setShowModal} />
+</Web3ReactProvider>
     </div>
   )
 }
