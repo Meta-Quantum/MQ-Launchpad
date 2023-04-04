e456b79dfb173d8207a96e51ea89e3e2d4e3d2d3
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-04-04 21:43:05 +0300

    Fixed some styling issues before netlify deployment

--- src/components/CoinTable/CoinTable.tsx
+++ src/components/CoinTable/CoinTable.tsx
@@ -1,48 +1,57 @@
-import React from 'react'
-import tableData from '../../api/mockup-table'
-import loupe from '../../assets/loupe_icon.png';
-import './CoinTable.scss'
+import React from "react";
+import tableData from "../../api/mockup-table";
+import loupe from "../../assets/loupe_icon.png";
+import "./CoinTable.scss";
+import { useNavigate } from "react-router-dom";
 
 function CoinTable() {
+  const navigate = useNavigate();
   return (
-    <div className='table__container'>
-    <div className='table__container__search'>
-        <a className='table__container__search__link' href='#'>view all</a>
-        <div className='table__container__search__bar'>
-            <img className='table__container__search__bar__icon' src={loupe} alt='loupe_icon' />
-            <input className='table__container__search__bar__input' placeholder='search'  type='text'/>
+    <div className="table__container">
+      <div className="table__container__search">
+        <a
+          href="#"
+          className="table__container__search__link"
+          onClick={() => {
+            navigate("/projects");
+          }}
+        >
+          view all
+        </a>
+        <div className="table__container__search__bar">
+          <img className="table__container__search__bar__icon" src={loupe} alt="loupe_icon" />
+          <input className="table__container__search__bar__input" placeholder="search" type="text" />
         </div>
-    </div>
+      </div>
 
-        <table className='table__container__table'>
-            <tr className='table__container__table__row'>
-                <th className='table__container__table__row__head'>Projects</th >
-                <th className='table__container__table__row__head'>SHO Total Raise </th >
-                <th className='table__container__table__row__head'>All Time High</th >
-                <th className='table__container__table__row__head'>Lead</th >
-                <th className='table__container__table__row__head'>Marketmakers</th >
-                <th className='table__container__table__row__head'>Type</th >
-            </tr>
-            {tableData.map((coin)=>(
-                <tr className='table__container__table__rowdata'>
-                    <td className='table__container__table__rowdata__name'> 
-                    <div className='table__container__table__rowdata__name__photo'>{coin.initialCoin}</div> 
-                    <div className='table__container__table__rowdata__name__element'>
-                        <p className='table__container__table__rowdata__name__element__text'>{coin.projectName}</p>
-                    <span className='table__container__table__rowdata__name__element__coin'>{coin.projectCoin}</span>
-                    </div>
-                    
-                    </td>
-                    <td className='table__container__table__rowdata__data'>{coin.SHO} $</td>
-                    <td className='table__container__table__rowdata__data'>{coin.allTimeHigh}</td>
-                    <td className='table__container__table__rowdata__data'>{coin.lead}</td>
-                    <td className='table__container__table__rowdata__data'>{coin.marketMakers}</td>
-                    <td className='table__container__table__rowdata__data'>{coin.type}</td>
-                </tr>
-            ))}
-        </table>
-        </div>
-  )
+      <table className="table__container__table">
+        <tr className="table__container__table__row">
+          <th className="table__container__table__row__head">Projects</th>
+          <th className="table__container__table__row__head">SHO Total Raise </th>
+          <th className="table__container__table__row__head">All Time High</th>
+          <th className="table__container__table__row__head">Lead</th>
+          <th className="table__container__table__row__head">Marketmakers</th>
+          <th className="table__container__table__row__head">Type</th>
+        </tr>
+        {tableData.map((coin) => (
+          <tr className="table__container__table__rowdata">
+            <td className="table__container__table__rowdata__name">
+              <div className="table__container__table__rowdata__name__photo">{coin.initialCoin}</div>
+              <div className="table__container__table__rowdata__name__element">
+                <p className="table__container__table__rowdata__name__element__text">{coin.projectName}</p>
+                <span className="table__container__table__rowdata__name__element__coin">{coin.projectCoin}</span>
+              </div>
+            </td>
+            <td className="table__container__table__rowdata__data">{coin.SHO} $</td>
+            <td className="table__container__table__rowdata__data">{coin.allTimeHigh}</td>
+            <td className="table__container__table__rowdata__data">{coin.lead}</td>
+            <td className="table__container__table__rowdata__data">{coin.marketMakers}</td>
+            <td className="table__container__table__rowdata__data">{coin.type}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
 
-export default CoinTable
+export default CoinTable;
